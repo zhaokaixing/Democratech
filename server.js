c20951081acffa7bdc22f1a4b66e084f6623f705
Florent AUPLAT <florent.auplat@gmail.com>
2017-02-16 12:55:11 +0100

    setup node server & client

---
+++ server.js
@@ -0,0 +1,29 @@
+var express     = require('express');
+var path        = require('path');
+var bodyParser  = require('body-parser');
+
+var port = 3000;
+
+var index = require('./routes/index');
+
+var app = express();
+
+// View engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', index);
+
+app.listen(port, function(){
+    console.log('server start on port '+port);
+});
+
+ 
