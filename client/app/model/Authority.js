f8df9702d16858cefc583af52c3915cb2b712486
inglo <quentin.coursimault@gmail.com>
2017-02-22 14:18:07 +0100

    /model:
    Je pense qu'il y a tout

---
+++ client/app/model/Authority.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * Created by quentinC on 22/02/2017.
+ */
+var Authority = (function () {
+    function Authority() {
+    }
+    return Authority;
+}());
+exports.Authority = Authority;
+//# sourceMappingURL=Authority.js.map
---
+++ client/app/model/Project.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * Created by quentinC on 20/02/2017.
+ */
+var Project = (function () {
+    function Project() {
+    }
+    return Project;
+}());
+exports.Project = Project;
+//# sourceMappingURL=Project.js.map
