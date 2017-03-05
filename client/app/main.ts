037d1ff6572a641139bc6a4fafe73e6d748eeef7
Florent AUPLAT <florent.auplat@gmail.com>
2017-03-05 12:51:03 +0100

    create home page & add authentication auth0 module && add express-jwt to server

--- client/app/main.ts
+++ client/app/main.ts
@@ -1,4 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule }              from './app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(success => console.log(`Bootstrap success`))
+  .catch(error => console.log(error));
