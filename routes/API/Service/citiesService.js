453c12c2ee448f9d71e6f7c3b6dbec5cc7ea56d1
inglo <quentin.coursimault@gmail.com>
2017-03-09 15:11:52 +0100

    add Service folder for create services

---
+++ routes/API/Service/citiesService.js
@@ -0,0 +1,23 @@
+/**
+ * Created by quentinC on 09/03/2017.
+ */
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class citiesService {
+    constructor() {
+        console.log('Task Service Initialized...');
+    }
+
+    getCities() {
+        return this.http.get('/api/cities')
+            .map(res => res.json());
+    }
+
+    getCity(id) {
+        return this.http.get('/api/city/'+id)
+            .map(res => res.json());
+    }
+}
