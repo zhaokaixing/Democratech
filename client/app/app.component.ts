503b9718385d5c29c7e73d4b61d8f7434c868622
Antoine <antoine.bernier@etu.univ-tours.fr>
2017-03-09 04:51:45 -0500

    Début implémentation html

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -5,7 +5,7 @@ import { AuthService } from './auth.service';
   moduleId: module.id,
   selector: 'my-app',
   templateUrl: 'views/app.component.html',
-  styles : ['.navbar-right { margin-right: 0px !important}']
+  styleUrls : ['views/styles/home.component.styles.css']
 })
 
 export class AppComponent { 
