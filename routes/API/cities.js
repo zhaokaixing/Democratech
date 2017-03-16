ea4c3a4b1d20cb463cfe191cf899775c5302ecff
inglo <quentin.coursimault@gmail.com>
2017-03-16 12:23:23 +0100

    Find cities by their department, OK !

--- routes/API/cities.js
+++ routes/API/cities.js
@@ -9,6 +9,27 @@ let db = mongojs('mongodb://florent:adelaide@ds113580.mlab.com:13580/democratch'
 
 
 
+
+var MongoClient = require('mongodb').MongoClient,
+    test = require('assert');
+
+router.get('/cities', function(req, res, next) {
+    MongoClient.connect('mongodb://florent:adelaide@ds113580.mlab.com:13580/democratch', function (err, db2) {
+
+        var collection = db2.collection('cities');
+
+        collection.find().toArray(function (err, docs) {
+            if(err){
+                res.send(err);
+            }
+            res.json(docs);
+            console.log(docs.length);
+            db2.close();
+        });
+    });
+});
+
+/*
 //get all cities
 router.get('/cities', function(req, res, next){
     db.cities.find(function(err, cities){
@@ -19,18 +40,26 @@ router.get('/cities', function(req, res, next){
     });
 });
 
-var MongoClient = require("mongodb").MongoClient;
-MongoClient.connect("mongodb://florent:adelaide@ds113580.mlab.com:13580/democratch", function(error, db) {
-    if (error) return funcCallback(error);
-    db.collection("cities").find().toArray(function (error, results) {
-        if (error) throw error;
+*/
 
-        results.forEach(function(i, obj) {
-            console.log(
-                "ID : "  + obj._id.toString() + "\n");
+var MongoClient = require('mongodb').MongoClient,
+    test = require('assert');
+
+router.get('/city/:id', function(req, res, next) {
+    MongoClient.connect('mongodb://florent:adelaide@ds113580.mlab.com:13580/democratch', function (err, db2) {
+
+        var collection = db2.collection('cities');
+
+        collection.findOne({_id : mongojs.ObjectId(req.params.id)},function (err, doc) {
+            if(err){
+                res.send(err);
+            }
+            res.json(doc);
+            db2.close();
         });
     });
 });
+/*
 //get one city
 router.get('/city/:id', function(req, res, next){
     db.cities.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, city){
@@ -39,8 +68,27 @@ router.get('/city/:id', function(req, res, next){
         }
         res.json(city);
     });
-});
+});*/
 
+var MongoClient = require('mongodb').MongoClient,
+    test = require('assert');
+
+router.get('/cities/:dep', function(req, res, next) {
+    MongoClient.connect('mongodb://florent:adelaide@ds113580.mlab.com:13580/democratch', function (err, db2) {
+
+        var collection = db2.collection('cities');
+        collection.find({'department' : parseInt(req.params.dep.valueOf())}).toArray(function(err, docs){
+            if(err){
+                console.log(err.message);
+                res.send(err);
+            }
+            res.json(docs);
+            console.log("Fini");
+            db2.close();
+        });
+    });
+});
+/*
 //get all city with department number
 router.get('/cities/:dep',function(req, res, next){
     db.cities.find({department : req.params.dep}, function(err, cities){
@@ -50,7 +98,7 @@ router.get('/cities/:dep',function(req, res, next){
         }
         res.json(cities);
     });
-});
+});*/
 
 
 module.exports = router
