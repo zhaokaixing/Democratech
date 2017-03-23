f7ef2db4216e5c66705898215a3ba316a6633bcc
Florent AUPLAT <florent.auplat@gmail.com>
2017-03-23 13:03:28 +0100

    add profile component

---
+++ client/app/profile.component.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by flwrnt on 23/03/2017.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'profile',
+  templateUrl: '',
+  styleUrls : ['']
+})
+
+export class ProfileComponent {
+  title = 'Profile';
+}
