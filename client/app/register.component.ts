d18444244780a807bd5e2858163e57a0f266d7a6
Florent AUPLAT <florent.auplat@gmail.com>
2017-03-23 13:00:24 +0100

    add register component & html

--- client/app/register.component.ts
+++ client/app/register.component.ts
@@ -1,3 +1,15 @@
 /**
  * Created by flwrnt on 23/03/2017.
  */
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'register',
+  templateUrl: 'views/register.component.html',
+  styleUrls : ['']
+})
+
+export class RegisterComponent {
+  title = 'Inscription';
+}
