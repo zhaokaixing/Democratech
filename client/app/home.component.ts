420ca052cae5df5cc846a3cfeda177828daef809
Koutatah <antoine.bernier59@gmail.com>
2017-03-30 05:03:39 -0400

    modify html app.component

--- client/app/home.component.ts
+++ client/app/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
 import {Project} from'./model/Project';
 
+
 @Component({
   moduleId: module.id,
   selector: 'home',
@@ -11,10 +12,14 @@ import {Project} from'./model/Project';
 })
 
 export class HomeComponent {
+
   PROJECTS: Project[] = [
     {id: 1, image: '/ressources/images/ecole.jpg' , label: 'Construction', description: 'Construction d\'une école'},
     {id: 2, image: '/ressources/images/ecolo.jpg', label: 'Ecologie', description: 'Ouverture gîte écologique'},
     {id: 3, image: '/ressources/images/pont.jpg', label: 'Construction', description: 'Construction d\'un pont traversant la Loire'},
     {id: 4, image: '/ressources/images/route.jpg', label: 'Construction', description: 'Rénovation de l\'autoroute A10'}
   ];
+
+
+
 }
