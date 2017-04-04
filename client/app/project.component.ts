832863c8742c565bffa33d2f3f1042076913cb19
myriamllb <myriam.leliboux@etu.univ-tours.fr>
2017-04-04 22:01:13 +0200

    Ajout chat (début)

--- client/app/project.component.ts
+++ client/app/project.component.ts
@@ -4,6 +4,17 @@ import { ProjectService } from './service/project.service';
 import {Project} from'./model/Project';
 import 'rxjs/add/operator/switchMap';
 
+export class Message {
+    username: string;
+    hour:number;
+    minute:number;
+    message:string;
+    jour:number;
+    mois:number;
+    annee:number;
+
+}
+
 @Component({
     moduleId: module.id,
     selector: 'home',
@@ -12,16 +23,22 @@ import 'rxjs/add/operator/switchMap';
     providers: [ProjectService],
 })
 
+
 export class ProjectComponent implements OnInit{
 
     project: Project = new Project();
-    lat: number = 51.678418;
-    lng: number = 7.809007;
+    MessageList: Message[] = [
+        { username: "Marc",hour:13,minute:25,jour:15,mois:3,annee:2017,message:"Projet très utile"},
+        { username: "Marie",hour:15,minute:11,jour:15,mois:3,annee:2017,message:"Très bonne initiative."},
+        { username: "Lucas",hour:16,minute:52,jour:15,mois:3,annee:2017,message:"Depuis le temps !!!!"},
+        { username: "Alex",hour:17,minute:16,jour:15,mois:3,annee:2017,message:"Tout à fait d'accord avec Marie !"},
+        { username: "Claire",hour:18,minute:25,jour:15,mois:3,annee:2017,message:"Quand serons finis les travaux ?"},
+        { username: "Entreprise de construction Dupont",hour:18,minute:45,jour:15,mois:3,annee:2017,message:"Les travaux seront terminés avant les vacances de noel !" }
+    ];
     constructor(private projectService: ProjectService, private router: Router, private route: ActivatedRoute) {}
 
 
     ngOnInit(): void {
-
         this.route.params
                 .switchMap((params: Params) => this.projectService.getOne(params['id']))
                 .subscribe(project => {
