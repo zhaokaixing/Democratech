302deb3b66efa38482e64c0a9e513828b36b0670
ElChauve <florent.flecheau@etu.univ-tours.fr>
2017-05-11 13:57:11 +0200

    Inscriptions citizen et organisation fonctionnelles + améliorations expressions régulières

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, group} from '@angular/core';
 import { Router } from "@angular/router";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import {FormGroup, FormBuilder, Validators, FormControl} from "@angular/forms";
 
 import { FlashMessagesService } from "angular2-flash-messages";
 import { CityService } from "app/services/city.service";
@@ -19,14 +19,13 @@ import { Country } from "app/models/Country";
 })
 
 export class RegisterComponent implements OnInit {
-
   country: Country = new Country('France');
   registerUserForm : FormGroup;
   registerOrganisationForm : FormGroup;
 
-  constructor(private departmentService: DepartmentService, 
+  constructor(private departmentService: DepartmentService,
               private cityService: CityService,
-              private userService: UserService, 
+              private userService: UserService,
               private formBuilder: FormBuilder,
               private router: Router,
               private flashMessagesService: FlashMessagesService) {
@@ -34,22 +33,27 @@ export class RegisterComponent implements OnInit {
     this.registerUserForm = formBuilder.group({
 
       'lastName': [null, [Validators.required,
-        Validators.pattern('[a-zA-Z]*-* *[a-zA-Z]*')]],
+        Validators.pattern('[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ -]*')]],
 
       'name': [null, [Validators.required,
-       Validators.pattern('[a-zA-Z]*-* *[a-zA-Z]*')]],
+        Validators.pattern('[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ -]*')]],
 
       'email': [null, Validators.required],
 
+      /*matchingPassword: formBuilder.group({
+        password: ['', Validators.required],
+        passwordConfirmation: ['', Validators.required]
+      }, {validator: this.areEqual}),*/
+
       'password': [null, [Validators.required,
         Validators.minLength(6),
-        Validators.maxLength(12),
+        Validators.maxLength(15),
         //To improve
         Validators.pattern('[a-zA-Z]+[0-9]+')]],
 
       'passwordConfirmation' : [null, [Validators.required,
         Validators.minLength(6),
-        Validators.maxLength(12),
+        Validators.maxLength(15),
         Validators.pattern('[a-zA-Z]+[0-9]+')
       ]],
 
@@ -57,21 +61,21 @@ export class RegisterComponent implements OnInit {
       'month' : ['', Validators.required],
       'year' : ['', Validators.required],
 
-      'country' : ['', Validators.required],
+      'country' : [''],
 
-      'department' : ['', Validators.required],
+      'department' : [''],
 
-      'city' : [null, [Validators.required,
-        Validators.pattern('[a-zA-Z]*-* *[a-zA-Z]*-* *[a-zA-Z]*')]],
+      'city' : [null,
+        [Validators.pattern('[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ -]*')]],
 
-      'postalCode' : [null, [Validators.required,
-        Validators.pattern('[0-9]{5}')]],
+      'postalCode' : [null,[Validators.pattern('[0-9]{5}')]],
 
-      'streetNumber' : [null, Validators.required],
+      'streetNumber' : [null],
 
-      'streetName' : [null, [Validators.required,
-        Validators.pattern('[a-zA-Z]*-* *[a-zA-Z]*-* *[a-zA-Z]*-* *[a-zA-Z]*')]],
+      'streetName' : [null,
+        [Validators.pattern('[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ -]')]],
     })
+
 
     /**
      * ****************************************************************************
@@ -122,36 +126,36 @@ export class RegisterComponent implements OnInit {
 
     this.registerOrganisationForm = formBuilder.group({
       'name': [null, [Validators.required,
-          Validators.pattern('[a-zA-Z]*-* *[a-zA-Z]*')]],
+        Validators.pattern('[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ -]*')]],
 
-      'mail': [null, Validators.required],
+      'email': [null, Validators.required],
 
       'password': [null, [Validators.required,
-          Validators.minLength(6),
-          Validators.maxLength(12),
-          //To improve
-          Validators.pattern('[a-zA-Z]+[0-9]+')]],
+        Validators.minLength(6),
+        Validators.maxLength(12),
+        //To improve
+        Validators.pattern('[a-zA-Z]+[0-9]+')]],
 
       'passwordConfirmation' : [null, [Validators.required,
-          Validators.minLength(6),
-          Validators.maxLength(12),
-          Validators.pattern('[a-zA-Z]+[0-9]+')]],
+        Validators.minLength(6),
+        Validators.maxLength(12),
+        Validators.pattern('[a-zA-Z]+[0-9]+')]],
 
       'type': ['Public'],
 
-      'country' : ['', [Validators.required]],
+      'country' : [''],
 
-      'department' : ['', Validators.required],
+      'department' : [''],
 
-      'city' : [null, Validators.required],
+      'city' : [null],
 
-      'postalCode' : [null, [Validators.required,
-          Validators.pattern('[0-9]{5}')]],
+      'postalCode' : [null,
+        [Validators.pattern('[0-9]{5}')]],
 
-      'streetNumber' : [null, Validators.required],
+      'streetNumber' : [null],
 
-      'streetName' : [null, [Validators.required,
-          Validators.pattern('[a-zA-Z]*-* *[a-zA-Z]*-* *[a-zA-Z]*-* *[a-zA-Z]*')]],
+      'streetName' : [null,
+        [Validators.pattern('[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ -]*')]],
     })
   }
 
@@ -211,6 +215,34 @@ export class RegisterComponent implements OnInit {
         // console.log('success');
       }
       else this.flashMessagesService.show('Une erreur est survenue lors de l\'enregistrement.', { cssClass: 'alert-danger', timeout: 5000 });
-    }) 
-  } 
+    })
+
+  }
+ /*
+ private areEqual() {
+    return function (c: FormGroup) {
+      return (c.value == this.password.value) ? null : {'passwordMatch': {valid: false}};
+    }
+  }
+
+   private areEqual(group: ControlGroup) {
+    let val;
+    let valid = true;
+    for (name in group.controls) {
+      if (val === undefined) {
+        val = group.controls[name].value
+      } else {
+        if (val !== group.controls[name].value) {
+          valid = false;
+          break;
+        }
+      }
+    }
+    if (valid) {
+      return null;
+    }
+
+    return {
+      areEqual: true
+    };*/
 }
